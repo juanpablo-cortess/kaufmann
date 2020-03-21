8f525919162c66edcaabacd25a6bc8259747317d
Juan Pablo Cortes <jpcortes@MacBook-Pro-de-juan-3.local>
2020-03-21 12:11:09 -0300

    cambiar estructura del script

--- kaufmann.R
+++ kaufmann.R
@@ -13,68 +13,101 @@ library(openxlsx)
 ga_auth()
 
 kaufmann_id<-162969725
-start_date<-"2019-02-01"
-end_date<-"2019-12-31"
-necesary_metrics<-c("uniquePageviews")
-necesary_dimensions<-c("month","sourceMedium","landingPagePath","campaign")
+start_date<-"2020-01-01"
+end_date<-"2020-03-18"
+necesary_metrics<-c("totalEvents",'uniqueEvents')
+necesary_dimensions<-c("eventCategory",'eventAction')
 
-canales_pagados<-c("(Other)","Display","Paid Search","Social")
-canales_no_pagados<-c("Direct","Email","Organic Search","Referral","Referral","No Pagado","(Other)","Social")
-"de acuerdo a an??lisis desde analytics el 20,99% de los leads Others se asignar??n a pagado"
+#Con esto podemos ver los segmentos disponibles en la cuenta
+segs_conv_kaufmann <- ga_segment_list() %>% filter(str_detect(name,regex('conversiones cotizador general',ignore_case = TRUE)))
 
-canales_kaufmann<-google_analytics(kaufmann_id,
+seg_conv_autos <- segment_ga4("Conversión cotizador automóviles",segment_id = "gaid::0W5mf4QzTfKhlh3MXa6xGA")
+seg_conv_camiones<-segment_ga4("Conversión cotizador camiones",segment_id = "gaid::XhkADWCuRF6HyZk6QaAFbg")
+seg_conv_minibuses<-segment_ga4("Conversión cotizador minibuses",segment_id = "gaid::T-_qvVKcRQGVH1v3KdesYQ")
+seg_conv_post<-segment_ga4("Conversión cotizador post",segment_id = "gaid::jP8rmVMISGiTpuOimXRIxg")
+seg_conv_rodados<-segment_ga4("Conversión cotizador rodados",segment_id = "gaid::jvbiPB2cQWy-64pKcIxL0Q")
+seg_conv_buses<-segment_ga4("Conversión cotizador buses",segment_id = "gaid::tJYrnGkmSzexi52Jmefziw")
+seg_conv_accesorios<-segment_ga4("Conversión cotizador accesorios",segment_id = "gaid::DaEKNkSgSuygilyyJtj7pw")
+seg_conv_power<-segment_ga4("Conversión cotizador power",segment_id = "gaid::dDpL5v1cQbeBKSklJ6kGCA")
+
+#autos 1
+conv_cruce_autos<-google_analytics(kaufmann_id,
                                   date_range = c(start_date,end_date),
-                                  metrics = c('users'),
-                                  dimensions = c('channelGrouping'),
-                                  anti_sample = TRUE)
+                                  metrics = c('users',necesary_metrics),
+                                  dimensions = necesary_dimensions,
+                                  anti_sample = TRUE,
+                                  filtersExpression = 'ga:eventCategory==COTIZADOR GENERAL',
+                                  segments =seg_conv_autos)
+#camiones 2
+conv_cruce_camiones<-google_analytics(kaufmann_id,
+                                   date_range = c(start_date,end_date),
+                                   metrics = c('users',necesary_metrics),
+                                   dimensions = necesary_dimensions,
+                                   anti_sample = TRUE,
+                                   filtersExpression = 'ga:eventCategory==COTIZADOR GENERAL',
+                                   segments =seg_conv_camiones)
 
+#minubuses 3
+conv_cruce_minibuses<-google_analytics(kaufmann_id,
+                                      date_range = c(start_date,end_date),
+                                      metrics = c('users',necesary_metrics),
+                                      dimensions = necesary_dimensions,
+                                      anti_sample = TRUE,
+                                      filtersExpression = 'ga:eventCategory==COTIZADOR GENERAL',
+                                      segments =seg_conv_minibuses)
+#post 4
+conv_cruce_post<-google_analytics(kaufmann_id,
+                                      date_range = c(start_date,end_date),
+                                      metrics = c('users',necesary_metrics),
+                                      dimensions = necesary_dimensions,
+                                      anti_sample = TRUE,
+                                      filtersExpression = 'ga:eventCategory==COTIZADOR GENERAL',
+                                      segments =seg_conv_post)
 
-porcentaje_fb<-sum(filter(visitas_pws,facebook==TRUE)$uniquePageviews)/sum(visitas_pws$uniquePageviews)
-porcentaje_fb_carrusel<-sum(filter(visitas_pws,tipo_Ad=="Carrusel")$uniquePageviews)/sum(filter(visitas_pws,facebook==TRUE)$uniquePageviews)
+#rodados 5
+conv_cruce_rodados<-google_analytics(kaufmann_id,
+                                      date_range = c(start_date,end_date),
+                                      metrics = c('users',necesary_metrics),
+                                      dimensions = necesary_dimensions,
+                                      anti_sample = TRUE,
+                                      filtersExpression = 'ga:eventCategory==COTIZADOR GENERAL',
+                                      segments =seg_conv_rodados)
 
-visitas_repuestos<-google_analytics(kaufmann_id,
-                              date_range = c(start_date,end_date),
-                              metrics = necesary_metrics,
-                              dimensions = necesary_dimensions,
-                              anti_sample = TRUE,
-                              filtersExpression ='ga:pagePath=~landing-repuestos\\/$'
-)%>%
-  mutate(facebook=case_when(
-    str_detect(sourceMedium,regex('facebook',ignore_case = TRUE)) & str_detect(sourceMedium,'social',negate = TRUE)~TRUE,
-    TRUE~FALSE
-  ))%>%
-  mutate(tipo_Ad=case_when(
-    str_detect(sourceMedium,regex("Carrusel",ignore_case = TRUE))~"Carrusel",
-    str_detect(sourceMedium,regex("link_ads",ignore_case = TRUE))~"Link_Ads",
-    str_detect(sourceMedium,regex("video",ignore_case = TRUE))~'video ads',
-    TRUE~''
-  ))
+#buses 6
+conv_cruce_buses<-google_analytics(kaufmann_id,
+                                      date_range = c(start_date,end_date),
+                                      metrics = c('users',necesary_metrics),
+                                      dimensions = necesary_dimensions,
+                                      anti_sample = TRUE,
+                                      filtersExpression = 'ga:eventCategory==COTIZADOR GENERAL',
+                                      segments =seg_conv_buses)
 
-porcentaje_fb_repuestos<-sum(filter(visitas_repuestos,facebook==TRUE)$uniquePageviews)/sum(visitas_repuestos$uniquePageviews)
-porcentaje_fb_carrusel_repuestos<-sum(filter(visitas_repuestos,tipo_Ad=="Carrusel")$uniquePageviews)/sum(filter(visitas_repuestos,facebook==TRUE)$uniquePageviews)
-porcentaje_fb_video_repuestos<-sum(filter(visitas_repuestos,tipo_Ad=="video ads")$uniquePageviews)/sum(filter(visitas_repuestos,facebook==TRUE)$uniquePageviews)
-porcentaje_fb_link_ads_repuestos<-sum(filter(visitas_repuestos,tipo_Ad=="Link_Ads")$uniquePageviews)/sum(filter(visitas_repuestos,facebook==TRUE)$uniquePageviews)
+#accesorios 7
+conv_cruce_accesorios<-google_analytics(kaufmann_id,
+                                      date_range = c(start_date,end_date),
+                                      metrics = c('users',necesary_metrics),
+                                      dimensions = necesary_dimensions,
+                                      anti_sample = TRUE,
+                                      filtersExpression = 'ga:eventCategory==COTIZADOR GENERAL',
+                                      segments =seg_conv_accesorios)
 
-visitas_servicio<-google_analytics(kaufmann_id,
-                                    date_range = c(start_date,end_date),
-                                    metrics = necesary_metrics,
-                                    dimensions = necesary_dimensions,
-                                    anti_sample = TRUE,
-                                    filtersExpression ='ga:pagePath=~landing-servicio-extendido\\/$'
-)%>%
-  mutate(facebook=case_when(
-    str_detect(sourceMedium,regex('facebook',ignore_case = TRUE)) & str_detect(sourceMedium,'social',negate = TRUE)~TRUE,
-    TRUE~FALSE
-  ))%>%
-  mutate(tipo_Ad=case_when(
-    str_detect(sourceMedium,regex("Carrusel",ignore_case = TRUE))~"Carrusel",
-    str_detect(sourceMedium,regex("link_ads",ignore_case = TRUE))~"Link_Ads",
-    str_detect(sourceMedium,regex("video",ignore_case = TRUE))~'video ads',
-    TRUE~''
-  ))
+#power 8
+conv_cruce_power<-google_analytics(kaufmann_id,
+                                      date_range = c(start_date,end_date),
+                                      metrics = c('users',necesary_metrics),
+                                      dimensions = necesary_dimensions,
+                                      anti_sample = TRUE,
+                                      filtersExpression = 'ga:eventCategory==COTIZADOR GENERAL',
+                                      segments =seg_conv_power)
 
-porcentaje_fb_servicios<-sum(filter(visitas_servicio,facebook==TRUE)$uniquePageviews)/sum(visitas_servicio$uniquePageviews)
-porcentaje_fb_carrusel_servicios<-sum(filter(visitas_servicio,tipo_Ad=="Carrusel")$uniquePageviews)/sum(filter(visitas_servicio,facebook==TRUE)$uniquePageviews)
-porcentaje_fb_video_servicios<-sum(filter(visitas_servicio,tipo_Ad=="video ads")$uniquePageviews)/sum(filter(visitas_servicio,facebook==TRUE)$uniquePageviews)
-porcentaje_fb_link_ads_servicios<-sum(filter(visitas_servicio,tipo_Ad=="Link_Ads")$uniquePageviews)/sum(filter(visitas_servicio,facebook==TRUE)$uniquePageviews)
-su
+total_cruce<-rbind(conv_cruce_autos,
+                     conv_cruce_camiones,
+                     conv_cruce_minibuses,
+                     conv_cruce_post,
+                     conv_cruce_rodados,
+                     conv_cruce_buses,
+                     conv_cruce_accesorios,
+                     conv_cruce_power)
+
+tabla_resumen<-dcast(total_cruce,segment~eventAction,value.var = "totalEvents")
+
